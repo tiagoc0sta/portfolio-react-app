7d6b447d21078afdffe6b0789c55bd8f4351f917
Tiago Costa <tiagodpv@gmail.com>
2022-11-16 21:23:02 -0500

    add navbar elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,8 +6,36 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div>
-
+        <img src={Logo} alt="Logo Image" style={{width: '150px'}}/>
       </div>
+      
+      {/*menu*/}
+      <div>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+        </ul>
+      </div>
+
+      {/*Hamburger */}
+      <div className='hidden'>
+        <FaBars/>
+      </div>
+
+      {/*Mobile Menu*/}
+      <ul className='hidden'>
+      <li>Home</li>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Work</li>
+          <li>Contact</li>
+      </ul>
+
+      {/*Social Icons */}
+      <div className="hidden"></div>
       
     </div>
   )
