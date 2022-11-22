937b7dc14ae4f2c368a08ce217d0d83b0c187f60
Tiago Costa <tiagodpv@gmail.com>
2022-11-21 22:02:42 -0500

    contac detaisl

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,10 +3,13 @@ import React from 'react'
 const Contact = () => {
   return (
     <div name='contact' className='w-full h-screen bg-[#0a192f] flex justify-center items-center p-4'>
-      <form action="flex flex-col max-w-[600px] w-full">
+      <form action="" className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
-        <p>Contact</p>
+          <p className='text-4xl font-bold inline border-b-4 border-[#129E57] text-gray-300'>Contact</p>
+          <p className='text-gray-300 py-4'>// Submit the form below or shoot me an email - tiagodpv@gmail.com</p>
         </div>
+        <input type="text" placeholder='Name' name='name'/>
+        <input className='my-4' type="email" placeholder='Email' name='email'/>
         
       </form>
     </div>
