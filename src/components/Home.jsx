2a81cbcf330fbcc1814db1cd21ca6cedba8c7164
Tiago Costa <tiagodpv@gmail.com>
2022-11-23 20:12:40 -0500

    frontend

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
       <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
         <p className='text-[#129E57] font-bold '>Hi, my name is</p>
         <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Tiago Costa</h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Frontend Developer,</h2>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Front-End Developer,</h2>
         <p className='text-[#8892b0] py-4 max-w-[700px]'>I am specializing in building and design exceptional UI experiences. Currently, I am focused on building responsive web applications.</p>
         <div>
           <Link to="work" smooth={true} duration={500}>
