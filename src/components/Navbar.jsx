04aac3a994b5e1b8f255bc7665070e60da242732
Tiago Costa <tiagodpv@gmail.com>
2022-11-26 22:00:58 -0500

    add new logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0a192f] text-gray-300'>
       <div >
-        <img className='cursor-pointer' src={Logo} alt="Logo" style={{width: '180px'}} 
+        <img className='cursor-pointer' src={Logo} alt="Logo" style={{width: '70px'}} 
           onClick={()=> scroll.scrollToTop()}/>
       </div>
       
